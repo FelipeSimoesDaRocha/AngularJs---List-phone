7b8e60983fc917951f5f9b45079f99c68d8d692b
Felipe Simões da Rocha <Felipe.simoesdarocha@gmail.com>
2021-09-14 17:35:43 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,13 +2,30 @@ angular.module("listaTelefonica", []);
 angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope) {
   $scope.app = "Lista Telefônica";
     $scope.contatos = [
-        { nome: "Felipe", telefone: "33345065" },
-        { nome: "Pamela", telefone: "33345065" },
-        { nome: "Alessandro", telefone: "33345065" },
+        { nome: "Felipe", telefone: "33345065",cor:"blue"},
+        { nome: "Pamela", telefone: "33345065",cor:"yellow"},
+        { nome: "Alessandro" , telefone: "33345065",cor:"red"}
+      ];
+      $scope.operadoras = [
+      {nome :"Vivo" ,codigo : 10, categoria:"chip"},
+      {nome :"Oi Fibra" , codigo  : 20, categoria:"chip"},
+      {nome :"GVT" , codigo : 30, categoria:"chip"},
+      {nome :"Tim" , codigo : 40, categoria:"chip"},
+      {nome :"Claro" , codigo : 40, categoria:"chip"}
       ];
     $scope.addContato = function (contato) {
-      debugger
-      $scope.contatos.push(contato);
+    $scope.contatos.push(angular.copy(contato ));
       delete $scope.contato;
     };
+$scope.apagarContatos = function(contatos){
+  $scope.contatos = contatos.filter(function(contato){
+    if (!contato.selecionado) return contato;
   });
+};
+  $scope.isContatoSelecionado = function(contatos) {
+    return contatos.some(function(contato) {
+      return contato.selecionado;
+    });
+ 
+  };
+});
