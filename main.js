fa6399d90a6e6e672e2d41319aa81aeb8e5120bb
Felipe Simões da Rocha <Felipe.simoesdarocha@gmail.com>
2021-09-15 17:28:23 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
-angular.module("listaTelefonica", []);
+angular.module("listaTelefonica", ["ngMessages"]);
 angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope) {
   $scope.app = "Lista Telefônica";
     $scope.contatos = [
-        { nome: "Felipe", telefone: "33345065",cor:"blue"},
-        { nome: "Pamela", telefone: "33345065",cor:"yellow"},
-        { nome: "Alessandro" , telefone: "33345065",cor:"red"}
+        { nome: "Felipe", telefone: "3334-5065",data:new Date(), operadora:{nome :"Vivo" , codigo : 10, categoria:"chip"}, cor:"blue"},
+        { nome: "Pamela", telefone: "4002-8922",data:new Date(),operadora:{nome :"Vivo" , codigo : 10, categoria:"chip"},cor:"yellow"},
+        { nome: "Alessandro" , telefone: "3334-5065",data:new Date(),operadora:{nome :"Tim" , codigo : 10, categoria:"chip"},cor:"red"}
       ];
       $scope.operadoras = [
-      {nome :"Vivo" ,codigo : 10, categoria:"chip"},
-      {nome :"Oi Fibra" , codigo  : 20, categoria:"chip"},
-      {nome :"GVT" , codigo : 30, categoria:"chip"},
-      {nome :"Tim" , codigo : 40, categoria:"chip"},
-      {nome :"Claro" , codigo : 40, categoria:"chip"}
+      {nome :"Vivo" , codigo : 10, categoria:"chip",cor:"red"},
+      {nome :"Oi "  , codigo : 20, categoria:"chip"},
+      {nome :"GVT"  , codigo : 30, categoria:"chip"},
+      {nome :"Tim"  , codigo : 40, categoria:"chip"},
+      {nome :"Claro", codigo : 40, categoria:"chip"}
       ];
     $scope.addContato = function (contato) {
     $scope.contatos.push(angular.copy(contato ));
       delete $scope.contato;
+      $scope.contatoForm.$setPristine()
+
     };
 $scope.apagarContatos = function(contatos){
   $scope.contatos = contatos.filter(function(contato){
@@ -26,6 +28,9 @@ $scope.apagarContatos = function(contatos){
     return contatos.some(function(contato) {
       return contato.selecionado;
     });
- 
+ $scope.ordenarPor = function (campo) {
+  $scope.criterioDeOrdenacao  = campo;
+  $scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
+ }
   };
 });
